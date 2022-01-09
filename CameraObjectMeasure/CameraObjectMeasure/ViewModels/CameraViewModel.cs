37215dfa3a45b8088e10af8b563ebca301450083
maotterson <otterson.2@gmail.com>
2022-01-08 22:49:26 -0500

    add resources

---
+++ CameraObjectMeasure/CameraObjectMeasure/ViewModels/CameraViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace CameraObjectMeasure.ViewModels
+{
+    public class CameraViewModel : BaseViewModel
+    {
+        public CameraViewModel()
+        {
+            Title = "Camera";
+        }
+
+    }
+}
---
+++ CameraObjectMeasure/CameraObjectMeasure/Views/Camera.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CameraObjectMeasure.Views
+{
+    public partial class CameraPage : ContentPage
+    {
+        public CameraPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
