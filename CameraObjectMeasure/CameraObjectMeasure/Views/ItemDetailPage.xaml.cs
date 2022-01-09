b5b2634cbe3209a973819372c92ddab86d059e5d
maotterson <otterson.2@gmail.com>
2022-01-08 22:18:25 -0500

    Add project files.

---
+++ CameraObjectMeasure/CameraObjectMeasure/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using CameraObjectMeasure.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace CameraObjectMeasure.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
